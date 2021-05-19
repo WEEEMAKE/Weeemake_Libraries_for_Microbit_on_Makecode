c22d9040a5890c8630c787b71864af82dfbe1fe4
WEEEMAKE <48052006+WEEEMAKE@users.noreply.github.com>
2021-05-19 18:30:30 +0800

    Update WeOneWire.cpp

--- WeOneWire.cpp
+++ WeOneWire.cpp
@@ -1,22 +1,20 @@
 #include "WeOneWire.h"
-#include "pxt.h"
-#include <stdarg.h>
 using namespace pxt;
 
 MicroBitPin get_port(uint8_t index)
 {
-    if (index == 1){
-        return uBit.io.P13;
-    }
-    else if (index == 2){
-        return uBit.io.P14;
-    }
-    else if (index == 3){
-        return uBit.io.P15;
-    }
-    else if (index == 4){
-        return uBit.io.P16;
-    }
+	if (index == 1){
+		return uBit.io.P13;
+	}
+	else if (index == 2){
+		return uBit.io.P14;
+	}
+	else if (index == 3){
+		return uBit.io.P15;
+	}
+	else if (index == 4){
+		return uBit.io.P16;
+	}
 }
 
 uint8_t one_wire_reset(uint8_t pinNum)
@@ -24,41 +22,41 @@ uint8_t one_wire_reset(uint8_t pinNum)
     if(pinNum == 1){
         uBit.io.P13.setDigitalValue(0);
         uBit.io.P13.setDigitalValue(0);
-        target_wait_us(480);
+        for (volatile uint16_t i = 0; i < 540; i++);
         int b = uBit.io.P13.getDigitalValue();
-        target_wait_us(50);
+        for (volatile uint8_t i = 0; i < 50; i++);
         b = uBit.io.P13.getDigitalValue();
-        target_wait_us(80);
+        for (volatile uint8_t i = 0; i < 100; i++);
         return b;
     }
     else if(pinNum == 2){
         uBit.io.P14.setDigitalValue(0);
         uBit.io.P14.setDigitalValue(0);
-        target_wait_us(480);
+        for (volatile uint16_t i = 0; i < 540; i++);
         int b = uBit.io.P14.getDigitalValue();
-        target_wait_us(50);
+        for (volatile uint8_t i = 0; i < 50; i++);
         b = uBit.io.P14.getDigitalValue();
-        target_wait_us(80);
+        for (volatile uint8_t i = 0; i < 100; i++);
         return b;
     }
     else if(pinNum == 3){
         uBit.io.P15.setDigitalValue(0);
         uBit.io.P15.setDigitalValue(0);
-        target_wait_us(480);
+        for (volatile uint16_t i = 0; i < 540; i++);
         int b = uBit.io.P15.getDigitalValue();
-        target_wait_us(50);
+        for (volatile uint8_t i = 0; i < 50; i++);
         b = uBit.io.P15.getDigitalValue();
-        target_wait_us(80);
+        for (volatile uint8_t i = 0; i < 100; i++);
         return b;
     }
     else if(pinNum == 4){
         uBit.io.P16.setDigitalValue(0);
         uBit.io.P16.setDigitalValue(0);
-        target_wait_us(480);
+        for (volatile uint16_t i = 0; i < 540; i++);
         int b = uBit.io.P16.getDigitalValue();
-        target_wait_us(50);
+        for (volatile uint8_t i = 0; i < 50; i++);
         b = uBit.io.P16.getDigitalValue();
-        target_wait_us(80);
+        for (volatile uint8_t i = 0; i < 100; i++);
         return b;
     }
 }
@@ -119,7 +117,7 @@ void one_wire_write_byte(uint8_t pinNum,uint8_t value)
     if(pinNum == 1){
         for(i=0; i<8; i++){
             uBit.io.P13.setDigitalValue(0);
-            target_wait_us(5);
+            for (volatile uint8_t j = 0; j < 2;j++);
             if(value & 0x01){
                 uBit.io.P13.setDigitalValue(1);
             }
@@ -127,15 +125,15 @@ void one_wire_write_byte(uint8_t pinNum,uint8_t value)
                 uBit.io.P13.setDigitalValue(0);
             }
             value = value>>1;
-            target_wait_us(25);
+            for (volatile uint8_t j = 0; j < 32;j++);
             uBit.io.P13.setDigitalValue(1);
-            target_wait_us(2);
+            for (volatile uint8_t j = 0; j < 4;j++);
         }
     }
     else if(pinNum == 2){
         for(i=0; i<8; i++){
             uBit.io.P14.setDigitalValue(0);
-            target_wait_us(5);
+            for (volatile uint8_t j = 0; j < 2;j++);
             if(value & 0x01){
                 uBit.io.P14.setDigitalValue(1);
             }
@@ -143,15 +141,15 @@ void one_wire_write_byte(uint8_t pinNum,uint8_t value)
                 uBit.io.P14.setDigitalValue(0);
             }
             value = value>>1;
-            target_wait_us(25);
+            for (volatile uint8_t j = 0; j < 32;j++);
             uBit.io.P14.setDigitalValue(1);
-            target_wait_us(2);
+            for (volatile uint8_t j = 0; j < 4;j++);
         }
     }
     else if(pinNum == 3){
         for(i=0; i<8; i++){
             uBit.io.P15.setDigitalValue(0);
-            target_wait_us(5);
+            for (volatile uint8_t j = 0; j < 2;j++);
             if(value & 0x01){
                 uBit.io.P15.setDigitalValue(1);
             }
@@ -159,15 +157,15 @@ void one_wire_write_byte(uint8_t pinNum,uint8_t value)
                 uBit.io.P15.setDigitalValue(0);
             }
             value = value>>1;
-            target_wait_us(25);
+            for (volatile uint8_t j = 0; j < 32;j++);
             uBit.io.P15.setDigitalValue(1);
-            target_wait_us(2);
+            for (volatile uint8_t j = 0; j < 4;j++);
         }
     }
     else if(pinNum == 4){
         for(i=0; i<8; i++){
             uBit.io.P16.setDigitalValue(0);
-            target_wait_us(5);
+            for (volatile uint8_t j = 0; j < 2;j++);
             if(value & 0x01){
                 uBit.io.P16.setDigitalValue(1);
             }
@@ -175,9 +173,9 @@ void one_wire_write_byte(uint8_t pinNum,uint8_t value)
                 uBit.io.P16.setDigitalValue(0);
             }
             value = value>>1;
-            target_wait_us(25);
+            for (volatile uint8_t j = 0; j < 32;j++);
             uBit.io.P16.setDigitalValue(1);
-            target_wait_us(2);
+            for (volatile uint8_t j = 0; j < 4;j++);
         }
     }
     __enable_irq();
@@ -185,15 +183,15 @@ void one_wire_write_byte(uint8_t pinNum,uint8_t value)
 
 uint8_t one_wire_read_byte(uint8_t pinNum)
 {
-    __disable_irq();
+	__disable_irq();
     if(pinNum == 1){   
         uint8_t i,j=0,k=0;
         uBit.io.P13.getDigitalValue();
         for(i=0; i<8; i++){
             while(1== uBit.io.P13.getDigitalValue());
-            target_wait_us(30);
+            for (volatile uint8_t l = 0; l < 30;l++);
             j = uBit.io.P13.getDigitalValue();
-            target_wait_us(60);
+            for (volatile uint8_t l = 0; l < 50;l++);
             k = (j<<7)|(k>>1);
         }
         return(k);
@@ -203,9 +201,9 @@ uint8_t one_wire_read_byte(uint8_t pinNum)
         uBit.io.P14.getDigitalValue();
         for(i=0; i<8; i++){
             while(1== uBit.io.P14.getDigitalValue());
-            target_wait_us(30);
+            for (volatile uint8_t l = 0; l < 30;l++);
             j = uBit.io.P14.getDigitalValue();
-            target_wait_us(60);
+            for (volatile uint8_t l = 0; l < 50;l++);
             k = (j<<7)|(k>>1);
         }
         return(k);
@@ -215,9 +213,9 @@ uint8_t one_wire_read_byte(uint8_t pinNum)
         uBit.io.P15.getDigitalValue();
         for(i=0; i<8; i++){
             while(1== uBit.io.P15.getDigitalValue());
-            target_wait_us(30);
+            for (volatile uint8_t l = 0; l < 30;l++);
             j = uBit.io.P15.getDigitalValue();
-            target_wait_us(60);
+            for (volatile uint8_t l = 0; l < 50;l++);
             k = (j<<7)|(k>>1);
         }
         return(k);
@@ -227,9 +225,9 @@ uint8_t one_wire_read_byte(uint8_t pinNum)
         uBit.io.P16.getDigitalValue();
         for(i=0; i<8; i++){   
             while(1== uBit.io.P16.getDigitalValue());
-            target_wait_us(30);
+            for (volatile uint8_t l = 0; l < 30;l++);
             j = uBit.io.P16.getDigitalValue();
-            target_wait_us(60);
+            for (volatile uint8_t l = 0; l < 50;l++);
             k = (j<<7)|(k>>1);
         }
         return (k);
